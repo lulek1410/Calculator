ab76bc744ebdc05fd2d9a5d73eb3d187c95e2101
lulek1410 <jan.szewczynski@gmail.com>
2023-10-13 14:48:09 +0200

    use absolute paths instead of relative

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import styled from "@emotion/styled";
-import Keypad from "./components/Keypad/Keypad";
-import Screen from "./components/Screen/Screen";
-import Header from "./components/Header/Header";
+import Keypad from "components/Keypad/Keypad";
+import Screen from "components/Screen/Screen";
+import Header from "components/Header/Header";
 
 const Calculator = styled.div`
 	display: grid;
