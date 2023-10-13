868a5e0a6a2d8fd61e0d6e239364f6fc038bdc54
lulek1410 <jan.szewczynski@gmail.com>
2023-10-13 10:39:32 +0200

    Add Screen component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import styled from "@emotion/styled";
 import Keypad from "./components/Keypad/Keypad";
+import Screen from "./components/Screen/Screen";
+
+const Calculator = styled.div`
+	display: grid;
+	gap: 20px;
+`
 
 function App() {
-	return <Keypad />;
+	return (
+		<Calculator>
+			<Screen />
+			<Keypad />
+		</Calculator>
+	);
 }
 
 export default App;
---
+++ src/components/Screen/Screen.tsx
@@ -0,0 +1,16 @@
+import styled from "@emotion/styled";
+
+const StyledScreen = styled.div`
+	background-color: var(--screen-bg-1);
+  padding: 30px 20px;
+  border-radius: 8px;
+
+	text-align: end;
+	color: white;
+`;
+
+const Screen = () => {
+	return <StyledScreen>399,981</StyledScreen>;
+};
+
+export default Screen;
