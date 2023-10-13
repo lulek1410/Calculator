edec387c2245898ee63c787ce8df67d43a0e5dab
lulek1410 <jan.szewczynski@gmail.com>
2023-10-13 14:49:37 +0200

    setup redux for app theme

---
+++ src/store/reducers/ThemeSlice.ts
@@ -0,0 +1,34 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export type Theme = "1" | "2" | "3";
+
+interface ThemeSlice {
+	theme: Theme;
+}
+
+const initialState: ThemeSlice = {
+	theme: "1",
+};
+
+const ThemeSlice = createSlice({
+	name: "theme",
+	initialState,
+	reducers: {
+		nextTheme: (state) => {
+			switch (state.theme) {
+				case "1":
+					state.theme = "2";
+					break;
+				case "2":
+					state.theme = "3";
+					break;
+				case "3":
+					state.theme = "1";
+					break;
+			}
+		},
+	},
+});
+
+export const { nextTheme } = ThemeSlice.actions;
+export default ThemeSlice.reducer;
